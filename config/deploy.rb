12071e0a4fbf0966691329548592561cfd7d90f2
Marcel Canclini <marcel.canclini@gmail.com>
2012-11-03 11:35:01 +0100

    cap solr tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,10 @@ namespace :deploy do
     end
   end
 
+  task :setup_solr_data_dir do
+    run "mkdir -p #{shared_path}/solr/data"
+  end
+
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
@@ -66,5 +70,23 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
   before "deploy:setup", "assets:symlinks:setup"
   before "deploy:create_symlink", "assets:symlinks:update"
+  after 'deploy:setup', 'deploy:setup_solr_data_dir'
+end
 
+namespace :solr do
+  desc "start solr"
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr start --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "stop solr"
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec sunspot-solr stop --port=8983 --data-directory=#{shared_path}/solr/data --pid-dir=#{shared_path}/pids"
+  end
+  desc "reindex the whole database"
+  task :reindex, :roles => :app do
+    stop
+    run "rm -rf #{shared_path}/solr/data"
+    start
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake sunspot:solr:reindex"
+  end
 end
